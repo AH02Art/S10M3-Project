f1a06e01097c0fa6e1af718f223acea4de392d6c
Alex Handley <smithvillescholarsah@gmail.com>
2024-04-01 20:11:01 -0500

    S10M3-Project completed

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { toggleVisiblity, deleteQuote, highlightQuote, toggleAuthenticity } from "../state/quotesSlice.js";
 
 export default function Quotes() {
-  const quotes = [{ // ✨ `quotes` must come from the Redux store
-    id: 3,
-    quoteText: "Be yourself; everyone else is already taken.",
-    authorName: "Oscar Wilde",
-    apocryphal: false,
-  }]
-  const displayAllQuotes = true // ✨ `displayAllQuotes` must come from the Redux store
-  const highlightedQuote = 3 // ✨ `highlightedQuote` must come from the Redux store
+  const quotes = useSelector((items) => items.quotesState.quotes);
+  const displayAllQuotes = useSelector((items) => items.quotesState.displayAllQuotes);
+  const highlightedQuote = useSelector((items) => items.quotesState.highlightedQuote);
+  const dispatch = useDispatch();
 
   return (
     <div id="quotes">
@@ -27,9 +25,16 @@ export default function Quotes() {
                 <div>{qt.quoteText}</div>
                 <div>{qt.authorName}</div>
                 <div className="quote-buttons">
-                  <button onClick={() => {/* ✨ dispatch an action */ }}>DELETE</button>
-                  <button onClick={() => {/* ✨ dispatch an action */ }}>HIGHLIGHT</button>
-                  <button onClick={() => {/* ✨ dispatch an action */ }}>FAKE</button>
+                  <button onClick={() => {
+                    const actionToDispatch = deleteQuote(qt.id);
+                    dispatch(actionToDispatch);
+                  }}>DELETE</button>
+                  <button onClick={() => {
+                    dispatch(highlightQuote(qt.id))
+                  }}>HIGHLIGHT</button>
+                  <button onClick={() => {
+                    dispatch(toggleAuthenticity(qt.id)) 
+                  }}>FAKE</button>
                 </div>
               </div>
             ))
@@ -38,7 +43,7 @@ export default function Quotes() {
           !quotes?.length && "No quotes here! Go write some."
         }
       </div>
-      {!!quotes?.length && <button onClick={() => {/* ✨ dispatch an action */ }}>
+      {!!quotes?.length && <button onClick={() => dispatch(toggleVisiblity())}>
         {displayAllQuotes ? 'HIDE' : 'SHOW'} FAKE QUOTES
       </button>}
     </div>
--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,7 @@
-// ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit";
 
 let id = 1
-const getNextId = () => id++
+export const getNextId = () => id++
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -26,3 +26,52 @@ const initialState = {
     },
   ],
 }
+
+export const quotesSlice = createSlice({
+  name: "quotes",
+  initialState,
+  reducers: {
+    toggleVisiblity(state) {
+      state.displayAllQuotes = !state.displayAllQuotes;
+    },
+    deleteQuote(state, action) {
+      state.quotes = state.quotes.filter((items) => items.id !== action.payload);
+    },
+    toggleAuthenticity(state, action) {
+      const quoteEdit = state.quotes.find((items) => items.id === action.payload);
+      quoteEdit.apocryphal = !quoteEdit.apocryphal;
+    },
+    highlightQuote(state, action) {
+      if (state.highlightedQuote === action.payload) {
+        state.highlightedQuote = null;
+      } else {
+        state.highlightedQuote = action.payload;
+      };
+    },
+    createQuote: {
+      prepare({ authorName, quoteText }) {
+        return {
+          payload: {
+            authorName,
+            quoteText,
+            apocryphal: false,
+            id: getNextId()
+          }
+        }
+      },
+      reducer(state, action) {
+        state.quotes.push(action.payload);
+      }
+    }
+  }
+});
+
+export const {
+  toggleVisiblity,
+  deleteQuote,
+  toggleAuthenticity,
+  highlightQuote,
+  createQuote
+} = quotesSlice.actions;
+
+export default quotesSlice.reducer;
